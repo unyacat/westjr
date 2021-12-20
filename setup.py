230493d357cb479ddabff66bad90f5c7c8047f2a
unyacat <admin@unyacat.net>
2021-12-20 16:52:56 +0900

    version 0.3

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='WestJR',
-    version='0.2.0.2',
+    version='0.3',
     license='Unlicense',
     description='Handling of train operation information of JR West, a railroad company in Japan',
     long_description=long_description,
