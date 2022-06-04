5dd8c2231c7bcaec46681549e39a39febfe4dbc0
eggplants <w10776e8w@yahoo.co.jp>
2022-06-04 13:23:24 +0900

    add: typedict for trafficinfo

--- westjr/response_types/train_info.py
+++ westjr/response_types/train_info.py
@@ -1,11 +1,37 @@
 # [/api/v3/area_{AREA}_trafficinfo.json]
 from __future__ import annotations
 
-from typing import Any
-
 from typing_extensions import TypedDict
+
+Section = TypedDict(
+    "Section",
+    {
+        "from": str,
+        "to": str,
+    },
+)
+
+
+class Info_LineItem(TypedDict):
+    count: int
+    section: Section
+    status: str
+    mark: int
+    cause: str
+    transfer: bool
+    url: str
+
+
+class Info_ExpressItem(TypedDict):
+    count: int
+    unique: int
+    name: str
+    status: str
+    mark: int
+    cause: str
+    url: str
 
 
 class TrainInfo(TypedDict):
-    lines: dict[str, Any]
-    express: dict[str, Any]
+    lines: dict[str, Info_LineItem]
+    express: dict[str, Info_ExpressItem]
