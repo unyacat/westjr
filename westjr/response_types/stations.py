5c622a4f82c6f4c400aeff03b2a58ab986b9d9c3
unyacat <admin@unyacat.net>
2024-08-03 23:54:50 +0900

    sanyo1のレスポンスに対応 (Fix: pydantic_core._pydantic_core.ValidationError: 145 validation errors for Stations #99)

--- westjr/response_types/stations.py
+++ westjr/response_types/stations.py
@@ -12,7 +12,12 @@ class TransferItem(BaseModel):
     code: str
     link: Optional[str]
     linkCode: Optional[str]
+    note: Optional[str] = None
+    substitute: Optional[bool] = False
 
+class PairDisplayItem(BaseModel):
+    code: str
+    position: int
 
 class Info(BaseModel):
     name: str
@@ -20,18 +25,23 @@ class Info(BaseModel):
     stopTrains: Optional[List[int]]
     typeNotice: Optional[str]
     transfer: Optional[List[TransferItem]]
-    line: Optional[str]
-    pairDisplay: Optional[str]
-    lines: Optional[str]
-
+    line: Optional[str] = None
+    pairDisplay: Optional[PairDisplayItem] = None
+    lines: Optional[str] = None
+    colorCode: Optional[str] = None
+    transferIcons: Optional[List[int]] = None
+    notDisplayType: Optional[int] = None
+    notDisplayNotices: Optional[List[str]] = None
+    end: Optional[bool] = False
 
 class SideItem(BaseModel):
     type: int
     side: Optional[int]
     linkLine: Optional[str]
     linkStationCode: Optional[str]
-    line: Optional[str]
-    linkDirection: Optional[int]
+    line: Optional[str] = None
+    linkDirection: Optional[int] = None
+    colorCode: Optional[str] = None
 
 
 class Design(BaseModel):
